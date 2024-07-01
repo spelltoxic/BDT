47898a0c7661a8bb04d4ab3421873ea9edd21c44
spelltoxic <97426509+spelltoxic@users.noreply.github.com>
2024-07-01 22:38:00 +0530

    Delete Scala directory

--- Scala/2.WordLength.scala
+++
@@ -1,8 +0,0 @@
-//2. Write scala code to find the length of each word from the array.
-
-object WordLength{
-    def main(args:Array[String]): Unit={
-        val words=Array("This","is","a","scala","code")
-        words.foreach(word => println(s"Length of '$word' is ${word.length}"))
-    }
-}
